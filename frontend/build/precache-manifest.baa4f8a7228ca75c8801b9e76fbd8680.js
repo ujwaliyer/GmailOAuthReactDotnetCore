bed3cff08fb8f63c033161cccdda5612492f327f
Ujwal <ujwal.iyer@sap.com>
2021-01-01 15:41:36 +0530

    first checkin..

---
+++ frontend/build/precache-manifest.baa4f8a7228ca75c8801b9e76fbd8680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d4a8b869ee38376b977885c7fb32b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c714ac2fd9764a7177aa",
+    "url": "/static/css/main.13d93feb.chunk.css"
+  },
+  {
+    "revision": "0e6e1b6040f4826342f9",
+    "url": "/static/js/2.33862841.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.33862841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c714ac2fd9764a7177aa",
+    "url": "/static/js/main.915e4206.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
